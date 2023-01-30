db4e701003bef504c46b7f5eac8c83b1476791df
Enrique <enrique.barbosasilva@gmail.com>
2023-01-30 17:59:02 -0300

    feat: client-controllers-created

--- src/controllers/Client/client.controllers.ts
+++ src/controllers/Client/client.controllers.ts
@@ -1,1 +1,24 @@
 import { Request, Response } from "express";
+import createClientService from "../../services/Client/createClient.service";
+import listClientService from "../../services/Client/listClients.service";
+
+export class ClientControllers {
+  async create(req: Request, res: Response) {
+    const { name, email, password, telephone } = req.body;
+
+    const createdClient = await createClientService({
+      name,
+      email,
+      password,
+      telephone,
+    });
+
+    return res.status(201).json(createdClient);
+  }
+
+  async list(req: Request, res: Response) {
+    const clients = await listClientService();
+
+    return res.json(clients);
+  }
+}
