e1d8c839685f67589a946fcb371e2847f09ff1ad
Enrique <enrique.barbosasilva@gmail.com>
2023-01-30 15:50:50 -0300

    feat: initial-structure

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { handleErrors } from "./errors/handleError";
+import clientRoutes from "./routes/client.routes";
+
+let cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/test", clientRoutes);
+app.use(handleErrors);
+
+export default app;
