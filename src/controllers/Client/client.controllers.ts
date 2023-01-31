8bf03fda1915645ee4c13aaecd603a3292047b4c
Enrique <enrique.barbosasilva@gmail.com>
2023-01-31 16:04:09 -0300

    feat: updateClient-controller

--- src/controllers/Client/client.controllers.ts
+++ src/controllers/Client/client.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import createClientService from "../../services/Client/createClient.service";
 import listClientService from "../../services/Client/listClients.service";
+import updateClientService from "../../services/Client/updateClient.service";
 
 export class ClientControllers {
   async create(req: Request, res: Response) {
@@ -20,5 +21,20 @@ export class ClientControllers {
     const clients = await listClientService();
 
     return res.json(clients);
+  }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name, email, password, telephone } = req.body;
+
+    const updatedClient = await updateClientService({
+      id,
+      name,
+      email,
+      password,
+      telephone,
+    });
+
+    return res.status(202).json(updatedClient);
   }
 }
