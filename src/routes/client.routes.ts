ebbd10e6d9cc2056e38231cfddbd247521f6bc60
Enrique <enrique.barbosasilva@gmail.com>
2023-01-31 16:05:09 -0300

    feat: updateClient-route

--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -6,5 +6,6 @@ const clientController = new ClientControllers();
 
 routes.post("", clientController.create);
 routes.get("", clientController.list);
+routes.patch("/:id", clientController.update);
 
 export default routes;
