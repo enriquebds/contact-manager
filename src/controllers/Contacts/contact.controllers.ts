9c09e25689cb6e684eb07896fd8dba5000a29c22
Enrique <enrique.barbosasilva@gmail.com>
2023-01-31 21:08:57 -0300

    feat: updateContact-controller

--- src/controllers/Contacts/contact.controllers.ts
+++ src/controllers/Contacts/contact.controllers.ts
@@ -3,6 +3,7 @@ import createContactService from "../../services/Contacts/createContact.service"
 import deleteContactService from "../../services/Contacts/deleteContact.service";
 import listContactById from "../../services/Contacts/listContactById.service";
 import listContactsService from "../../services/Contacts/listContacts.service";
+import updateContactService from "../../services/Contacts/updateContact.service";
 
 export class ContactControllers {
   async create(req: Request, res: Response) {
@@ -41,5 +42,19 @@ export class ContactControllers {
     return res.json({
       message: "Contact deleted",
     });
+  }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name, email, telephone } = req.body;
+
+    const updatedClient = await updateContactService({
+      id,
+      name,
+      email,
+      telephone,
+    });
+
+    return res.status(202).json(updatedClient);
   }
 }
